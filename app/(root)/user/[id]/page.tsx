4316613a08f06fb11108f1d76ff256c98499baab
EssamSecond <essam@dhaka.edu>
2025-05-14 15:38:12 +0600

    Project Done

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -12,7 +12,6 @@ export const experimental_ppr = true;
 const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
   const session = await auth();
-  console.log("ID: " + id);
   const user = await client.fetch(AUTHOR_BY_ID_QUERY, { id });
 
   if (!user) return notFound();
